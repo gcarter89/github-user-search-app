ddaaadc126153a3df167e056f77e184d8eca425c
Gareth Carter <gareth.carter@outlook.com>
2022-04-20 00:24:16 +0100

    searchbar mobile styling completed

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,9 +1,10 @@
 import Card from "./Card";
 import SearchBar from "./SearchBar";
+import styles from './main.module.scss';
 
 export default function Main() {
     return (
-        <main>
+        <main className={styles.main}>
             <SearchBar />
             <Card />
         </main>
--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,7 +1,13 @@
+import styles from './searchbar.module.scss';
+import { ReactComponent as SearchIcon } from '../assets/icon-search.svg'
 export default function SearchBar() {
     return (
-        <div>
-            <textarea />
+        <div className={styles.searchbar}>
+            <div className={styles.searchbar_iconInput}>
+                <SearchIcon className={styles.searchbar_icon} />
+                <input placeholder='Search Github username…' className={styles.searchbar_input} />
+            </div>
+            <button className={styles.searchbar_button}>Search</button>
         </div>
         
     )
