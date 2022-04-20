f9886bd52a672a2e2cc4f164036ba62480f0a9cf
Gareth Carter <gareth.carter@outlook.com>
2022-04-20 19:35:18 +0100

    moved data fetch up one level in the component tree

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,18 +1,46 @@
 import Card from "./Card";
 import SearchBar from "./SearchBar";
 import styles from './main.module.scss';
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
+interface iUser {
+    name?: string,
+    login?:string,
+    public_repos?:number,
+    followers?:number,
+    following?:number,
+    blog?:string,
+    location?:string,
+    company?:string,
+    twitter_username?:string,
+    avatar_url?:string,
+    created_at:string,
+    bio?:string
+}
 
 
 export default function Main() {
 
     const [username, setUsername]= useState('octocat');
+    const [data, setData] = useState<iUser>({created_at:""});
+
+    async function fetchUser(user:string) {
+        const response = await fetch(`https://api.github.com/users/${user}`);
+        const result = await response.json();
+        return await result;
+    }
+
+    useEffect(() => { 
+        fetchUser(username).then(function(result) {
+            setData(result)
+        })
+    },[username])
+
 
     return (
         <main className={styles.main}>
             <SearchBar setUsername={setUsername} />
-            <Card username={username} />
+            <Card user={data} />
         </main>
     )
 }
