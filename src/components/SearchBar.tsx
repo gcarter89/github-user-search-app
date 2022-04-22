4133d26c0b527b4c7862efddf231cec2a8175a20
Gareth Carter <gareth.carter@outlook.com>
2022-04-22 01:15:48 +0100

    completed searchbar styling for light and dark modes

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,6 +1,7 @@
 import styles from './searchbar.module.scss';
 import { ReactComponent as SearchIcon } from '../assets/icon-search.svg'
-import { useState } from 'react';
+import { useContext, useState } from 'react';
+import { ThemeContext } from '../App';
 
 interface iSearchBarProps {
     setUsername: React.Dispatch<React.SetStateAction<string>>
@@ -9,6 +10,8 @@ interface iSearchBarProps {
 
 
 const SearchBar: React.FC<iSearchBarProps> = ({setUsername, found}) => {
+
+    const {darkMode} = useContext(ThemeContext);
 
     const [input, setInput] = useState('');
 
@@ -22,9 +25,9 @@ const SearchBar: React.FC<iSearchBarProps> = ({setUsername, found}) => {
     }
 
     return (
-        <div className={styles.searchbar}>
+        <div className={darkMode ? `${styles.searchbar} ${styles.searchbar__dark}` : `${styles.searchbar} ${styles.searchbar__light}}`}>
                 <SearchIcon className={styles.searchbar_icon} />
-                <input onChange={(e) => handleInputChange(e)} placeholder='Search Github username…' className={styles.searchbar_input} />
+                <input onChange={(e) => handleInputChange(e)} placeholder='Search Github username…' className={darkMode ? `${styles.searchbar_input} ${styles.searchbar_input__dark}` : `${styles.searchbar_input} ${styles.searchbar_input__light}}`} />
                 {!found && <p className={styles.searchbar_error}><strong>No results</strong></p>}
             <button onClick={(e) => handleSubmit(e, input)} className={styles.searchbar_button}>Search</button>
         </div>
