96651ea7f8fede1e8ae3c6d4ac78980cc9caff5f
Gareth Carter <gareth.carter@outlook.com>
2022-04-23 13:31:06 +0100

    added span element for accessibility

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -19,7 +19,7 @@ export default function Header() {
         <header className={styles.header}>
             <h1 className={darkMode ? `${styles.header_title} ${styles.header_title__dark}` : `${styles.header_title} ${styles.header_title__light}`}>devfinder</h1>
             <button onClick={(e) => handleClick(e)} className={darkMode ? `${styles.header_button} ${styles.header_button__dark}` : `${styles.header_button} ${styles.header_button__light}`}>
-                <h4 className={styles.header_buttonCopy}>{darkMode ? 'LIGHT' : 'DARK'}</h4>
+                <span className={styles.header_buttonCopy}>{darkMode ? 'LIGHT' : 'DARK'}</span>
                 { darkMode ? <LightIcon /> : <DarkIcon /> }        
             </button>
         </header>
